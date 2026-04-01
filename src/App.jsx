d965922f1f440acc711f7fef85f949d81aeeb429
Abhii <prabheen09876@gmail.com>
2026-04-01 22:08:10 +0530

    feat: initialize app routing and migrate to lenis scroll library with legacy peer dependency support

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { ReactLenis } from '@studio-freight/react-lenis';
+import { ReactLenis } from 'lenis/react';
 import Home from './pages/Home';
 import About from './pages/About';
 import Booking from './pages/Booking';
@@ -8,21 +8,24 @@ import Gallery from './pages/Gallery';
 import CustomCursor from './components/CustomCursor';
 import Navbar from './components/Navbar';
 import './App.css';
+import 'lenis/dist/lenis.css';
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <CustomCursor />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/booking" element={<Booking />} />
-          <Route path="/gallery" element={<Gallery />} />
-        </Routes>
-      </div>
-    </Router>
+    <ReactLenis root>
+      <Router>
+        <div className="app">
+          <CustomCursor />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/booking" element={<Booking />} />
+            <Route path="/gallery" element={<Gallery />} />
+          </Routes>
+        </div>
+      </Router>
+    </ReactLenis>
   );
 }
 
